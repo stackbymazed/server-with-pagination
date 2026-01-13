fd09ac7aa0cc975524a59c3f63955715554a422b
Nayem783982 <majedulislam223311@gmail.com>
2026-01-13 13:44:11 +0600

    fixed the prisma error

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
-import { PrismaPg } from '@prisma/adapter-pg'
-import { PrismaClient } from '../../generated/prisma/client'
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-const connectionString = `${process.env.DATABASE_URL}`
+const connectionString = process.env.DATABASE_URL as string;
 
-const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+const adapter = new PrismaPg({ connectionString });
 
-export { prisma }
+const prisma = new PrismaClient({ adapter });
+
+export { prisma };
